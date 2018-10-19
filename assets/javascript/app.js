fef8ba69ad39d690c838f2cc6ba357bd16c04c0c
Nancyb <nancyeb27@yahoo.com>
2018-10-18 23:23:52 -0600

    added questions section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,119 @@
+// Question and answer Array
+var questions = [
+    {
+        question: 'What sports team is on the grandsons shirt?',
+        answers: [
+            { answer: "A. Broncos", value: false },
+            { answer: "B. Braves", value: false },
+            { answer: "C. Gaints", value: false },
+            { answer: "D. Bears", value: true },
+        ]
+    },
+    {
+
+        question: 'What sports team is on the grandsons shirt?',
+        answers: [
+            { answer: "A. Broncos", value: false },
+            { answer: "B. Braves", value: false },
+            { answer: "C. Gaints", value: false },
+            { answer: "D. Bears", value: true },
+        ]
+    },
+    {
+        question: 'What sports team is on the grandsons shirt?',
+        answers: [
+            { answer: "A. Broncos", value: false },
+            { answer: "B. Braves", value: false },
+            { answer: "C. Gaints", value: false },
+            { answer: "D. Bears", value: true },
+        ]
+    },
+    {
+        question: 'What sports team is on the grandsons shirt?',
+        answers: [
+            { answer: "A. Broncos", value: false },
+            { answer: "B. Braves", value: false },
+            { answer: "C. Gaints", value: false },
+            { answer: "D. Bears", value: true },
+        ]
+    },
+    {
+        question: 'What sports team is on the grandsons shirt?',
+        answers: [
+            { answer: "A. Broncos", value: false },
+            { answer: "B. Braves", value: false },
+            { answer: "C. Gaints", value: false },
+            { answer: "D. Bears", value: true },
+        ]
+    }
+];
+
+// Global variables
+var game;
+var counter = 0;
+var clock;
+var timer;
+var correctCounter = 0;
+var incorrectCounter = 0;
+var unansweredCounter = 0;
+
+
 $(document).ready(function () {
-
-    var gameStartTime = 45;
-    var timer;
-
-    $("#startgame").on("click", function () {
-        timer = setInterval(gameTimer, 1000);
-        gameTimer();
-        console.log(timer);
-        $("#startgame").remove();
-    })
-    function gameTimer() {
-        gameStartTime--;
-        $("#timeLeft").html(gameStartTime);
-        console.log(gameStartTime);
-        // if 
-            // stop game
-    }
-
-
-    // create questions and answers/chocies 
-    // create a tally with correct/incorrect/no answer 
-    // create a timer for each session
-    // only one answer allowed per question
-    // write/get bootstrap for html
-    // create a button to start the game
-    // start over button -does not reload page, it resets game/done button
-    function submitAnswers() {
-        var total = 6;
-        var score = 0;
-
-        // get user input
-        var q1 = document.forms["quizform"]['q1'].value;
-        var q2 = document.forms["quizform"]['q2'].value;
-        var q3 = document.forms["quizform"]['q3'].value;
-        var q4 = document.forms["quizform"]['q4'].value;
-        var q5 = document.forms["quizform"]['q5'].value;
-        var q6 = document.forms["quizform"]['q6'].value;
-
-        // validation
-        for (i = 1; i <= total; i++) {
-            if (eval("q" + i) = null || ("q" + i) == "") {
-                return false;
-
+    $(".answers").css("visibility", "hidden");
+    $("body").on("click", ".start-btn", function (event) {
+        event.preventDefault();
+        startGame();
+        $(".answers").css('visibiity', "visible");
+    });
+    $("body").on("click", ".answer", function (event) {
+        // console.log($(this));
+        chosenAnswer = $(this).text();
+        var answerCounter = questions[counter].answers;
+        var answer = $(".answer");
+        for (var i = 0; i < answerCounter.length; i++) {
+            if (chosenAnswer === answerCounter[i].answer && answerCounter[i].value === true) {
+                clearInterval(clock);
+                var right = $(this).attr('class', "right-answer answer");
+                rightAnswer();
+            }
+            else if (chosenAnswer === answerCounter[i].answer && answerCounter[i].value === flase) {
+                clearInterval(clock);
+                $(this).attr('class', 'wrong-answer answer');
+                wrongAnswer();
             }
         }
-        // set correct answers!!!!after questiosn set update!
-        var answers = ["a", "b", "c", "d", "e", "c"];
-
-        // check answers//change score to correct
-        for (i = 1; i <= total; i++) {
-            if (eval("q" + i) == answers[i - 1]) {
-                score++;
-                console.log("score");
-            }
-            //  display results
-            var results = document.getElementById('results');
-            results.innerHTML = "<h3> You scored <span> '+score+' </span> out of <span>'+total+'</span></h3>"   // alert("You scored " +score+ " out of +total+");
-        }
-
-        return false;
-
-    }
-
+    });
 });
+    
 
+
+            //  display results
+//             var results = document.getElementById('results');
+//             results.innerHTML = "<h3> You scored <span> '+score+' </span> out of <span>'+total+'</span></h3>"   // alert("You scored " +score+ " out of +total+");
+//         }
+
+//         return false;
+
+//     }
+
+// });
+
+// var gameStartTime = 45;
+
+
+// $("#startgame").on("click", function () {
+//     timer = setInterval(gameTimer, 1000);
+//     gameTimer();
+//     console.log(timer);
+//     $("#startgame").remove();
+// })
+// function gameTimer() {
+//     // if gameStartTime >= 0;
+//     gameStartTime--;
+//     $("#timeLeft").html("Time Remaining " + gameStartTime);
+//     console.log(gameStartTime);
+    //  else  {
+    //      if gameStartTime == 0;
+    //         end gameStartTime;
+    //  }  
+        // stop game
