86a37d429f03ec45644d9cd2cd8bc1415e476d2e
Nancyb <nancyeb27@yahoo.com>
2018-10-20 21:39:44 -0600

    Added Read Me File

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # TriviaGame
 
-You'll create a trivia form with multiple choice or true/false options (your choice).
 
-The player will have a limited amount of time to finish the quiz. 
+Welcome to the "Breakfast at Tiffany's" trivia game.
+
+You will be shown only one question at a time until the you answers it or your time runs out.
+You will have 30 seconds to answer each question.
+
+If the you select the correct answer, a screen tell you pop with with Correct! then after a few seconds,  then next question will be displayed.
+
+If you guess wrong or your time runs out, a screen will pop up to show you the correct answer, then next question will be displayed.
+
+After 5 questions, the final screen will show you the number of correct answers, incorrect answers, and an option to restart the game.
+
+Thank you for playing.
 
 
-The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
-
-
-Don't let the player pick more than one answer per question.
-Don't forget to include a countdown timer.
+Style choose: This games main theme is in Black and White to reflect the famous black dress and hat Holly wears in the movie.  
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,227 +1,205 @@
 // Question and answer Array
-$(document).ready (function () {
+$(document).ready(function () {
 
 
     var questions = [
-    {
-        question: 'What is the name of the first man Holly is determined to marry--the ninth richest man in America under fifty?',
-        answers: ["Rusty Trawler", "Braves","Gaints","Bears"],
-        values: [true, false, false, false] 
-    },
-    {
+        {
+            question: "Who is Holly determined to marry--the ninth richest man in America under fifty?",
+            answers: ["Rusty Trawler", "Blake Edwards", "Rock Hudson", "Jose de Silva Perrera"],
+            values: [true, false, false, false],
+            
+        },
+        {
 
-        question: "I claimed to be Paul's 'decorator'. Who played me?",
-        answers: ["Broncos", "Patricia Neal ","Gaints","Bears"],
-        values: [false, true, false, false] 
-    },
-    {
-        question: 'When Doc is trying to convince Lulamae/Holly to come back to Texas with him, when does he say that Fred is due to get out of the Army?',
-        answers: ["Broncos", "Braves","Gaints","February "],
-        values: [false, false, false, true] 
-    },
-    {
-        question: "How much do Holly's men give her for the powder room?",
-        answers: ["Broncos", "Braves","Gaints","50 dollars"],
-        values: [false, false, false, true] 
-    },
-    {
-        question: "Where is Tiffany's located?",
-        answers: ["Broncos", "Braves","Gaints","New York"],
-        values: [false, false, false, true] 
-    }
-];
-
-// Global variables
-var currentQuestion = 0;
-var correct = 0;
-var wrong = 0;
-var none = 0;
-
-   
-    $("#start").on("click", function () {
+            question: "What is Holly's cat's name?",
+            answers: ["Queenie", "Cat", "Sherman", "Mr. Peabody"],
+            values: [false, true, false, false],
         
+                },
+        {
+            question: 'When Doc is trying to convince Lulamae/Holly to come back to Texas with him, when does he say that Fred is due to get out of the Army?',
+            answers: ["June", "April", "October", "February "],
+            values: [false, false, false, true],
+           
+        },
+        {
+            question: "Where does Holly retreat when things get awkward between her and Paul?",
+            answers: ["The library", "The bathroom", "The mall", "Tiffany's"],
+            values: [true, false, false, false],
+           
+        },
+        {
+            question: "What famous actor played the small role of Mr. Yunioshi?",
+            answers: ["Mickey Rooney", "Henry Fonda", "Gene Kelly", "Clark Gable"],
+            values: [true, false, false, false],
+            
+        }
+    ];
+
+    // Global variables
+    var currentQuestion = 0;
+    var correct = 0;
+    var wrong = 0;
+    var none = 0;
+
+
+    $("#start").on("click", function () {
+
         $("#start").fadeToggle("slow", displayQ)
     })
-    function displayQ(){
+    function displayQ() {
         $(".message-content").remove();
-    $("#start").remove();
+        $("#start").remove();
 
-        
-       var questionArea = $("<div>");
-       questionArea.attr("id", "question-Area")
-       var timer = $("<h2>")
-       var question = $("<h2>")
 
-       questionArea.appendTo("#content")
-       timer.appendTo(questionArea)
-       question.appendTo(questionArea)
+        var questionArea = $("<div>");
+        questionArea.attr("id", "question-Area")
+        var timer = $("<h2>")
+        var question = $("<h2>")
 
-       var time = 30;
-       timer.html("<h2>" + time + "  seconds remaining</h2>")
+        questionArea.appendTo("#content")
+        timer.appendTo(questionArea)
+        question.appendTo(questionArea)
 
-       var countDown = setInterval( function() {
-           time--;
-           timer.html("<h2>" + time + " seconds remaining</h2>")
+        var time = 30;
+        timer.html("<h2>" + time + "  Seconds Remaining</h2>")
 
-           if (time === 0) {
-               clearInterval(countDown)
-               questionArea.fadeToggle("slow", timedOut)
-               none ++;
+        var countDown = setInterval(function () {
+            time--;
+            timer.html("<h2>" + time + " Seconds Remaining</h2>")
+
+            if (time === 0) {
+                clearInterval(countDown)
+                questionArea.fadeToggle("slow", timedOut)
+                none++;
             }
-       }, 1000);
+        }, 1000);
 
-       question.html(questions[currentQuestion].question)
+        question.html(questions[currentQuestion].question)
 
-            for (var i = 0; i < questions [currentQuestion].answers.length; i++) {
-                var answers = $("<button>")
-                answers.html(questions[currentQuestion].answers[i])
-                answers.addClass("answer-button")
-                answers.attr("value", questions[currentQuestion].values[i])
-                answers.appendTo(questionArea)
-            };
+        for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
+            var answers = $("<button>")
+            answers.html(questions[currentQuestion].answers[i])
+            answers.addClass("answer-button")
+            answers.attr("value", questions[currentQuestion].values[i])
+            answers.appendTo(questionArea)
+        };
 
-            $("#a0").animate({"left":  " +=600px"})
+        // $("#a0").animate({ "left": " +=600px" })
 
-            $(".answer-button").on ("click", function(){
-                console.log($(this).attr("value"));
+        $(".answer-button").on("click", function () {
+            console.log($(this).attr("value"));
 
-            if ($(this).attr("value") === "true"){
+            if ($(this).attr("value") === "true") {
                 questionArea.fadeToggle("slow", displayCorrect)
                 clearInterval(countDown);
-                correct ++;
+                correct++;
             };
-      
-             if ($(this).attr("value") === "flase"){
-                questionArea.fadeToggle("slow", displayCorrect)
+
+            if ($(this).attr("value") === "false") {
+                questionArea.fadeToggle("slow", displayWrong)
                 clearInterval(countDown);
-                wrong ++;
+                wrong++;
             };
         });
     };
 
-        function displayCorrect() {
-            var cycle = setTimeout(displayQ, 5000)
-            var messageArea = $("<div>");
-            messageArea.addClass("message-content")
+    function displayCorrect() {
+        var cycle = setTimeout(displayQ, 5000)
+        var messageArea = $("<div>");
+        messageArea.addClass("message-content")
 
-            var winMessage = $("<h2>");
-            var detail = $("<h2>");
+        var winMessage = $("<h2>")
+       
 
-            messageArea.appendTo($('#content'));
-            winMessage.appendTo($(messageArea));
-            winMessage.text("Correct!");
-            
-        if (currentQuestion === (questions.length -1)) {
-            clearTimeout (cycle);
-            var gameEnd = setTimeout ( gameOver, 5000)
-        }
-        currentQuestion ++;
-        };
-            
-        function displayWrong() {
-            var cycle = setTimeout(displayQ, 5000)
-            var messageArea = $("<div>");
-            messageArea.addClass("message-content")
-
-            var lossMessage = $("<h2>");
-            
-            messageArea.appendTo($('#content'));
-            lossMessage.appendTo($(messageArea));
-            lossMessage.html("Wrong! The right answer was: " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
-           
-
-        if (currentQuestion === (questions.length -1)) {
-            clearTimeout (cycle);
-            var gameEnd = setTimeout ( gameOver, 5000)
-        }
-        currentQuestion ++;
-        };
-
-        function timedOut(){
-            cycle = setTimeout(displayQ, 5000)
-            messageArea = $("<div>");
-            messageArea.addClass("message-content")
-            var lossMessage = $("<h2>");
-
-            messageArea.appendTo($("#content"));
-            lossMessage.appendTo(messageArea)
-            lossMessage.html("Wrong! The right answer was: " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
+        messageArea.appendTo($('#content'));
+        winMessage.appendTo($(messageArea));
       
-            if (currentQuestion === (questions.length -1)){
-                clearInterval (cycle);
-                var gameEnd = setTimeout( gameOver, 5000)
-            }
-            currentQuestion ++;
+        winMessage.text("Correct!");
+      
 
-        };
 
-        function gameOver() {
-            $(".message-content").remove();
-            var totalCorrect = $("<h3>")
-            var totalIncorrect = $("<h3>")
-            var totalNone = $("<h3>")
-            var restart = $("<button>")
-            totalCorrect.appendTo($("#content"))
-            totalCorrect.html("You got " + correct + " correct!")
-            totalIncorrect.appendTo("#content")
-            totalIncorrect.html("You got " + wrong + " wrong.")
-            totalNone.appendTo("#content")
 
-            if (none === 1) {
-                totalNone.html ("You didn't answer  " + none + " question.")
-            }
-                if (none > 1 || none === 0) {
-                    totalNone.html ("You didn't answer  " + none + " questions.")
-                } 
-                restart.addClass("restart")
-                restart.text("Restart")
-                restart.appendTo($("#content"))
+        if (currentQuestion === (questions.length - 1)) {
+            clearTimeout(cycle);
+            var gameEnd = setTimeout(gameOver, 2000)
+        }
+        currentQuestion++;
+    };
 
-                $(".restart").on ("click" , function() {
-                    totalCorrect.remove();
-                        totalIncorrect.remove();
-                        totalNone.remove();
-                        restart.remove();
-                        currentQuestion = 0;
-                        correct = 0;
-                        wrong =0;
-                        none = 0;
-                        displayQ();
-                })
-            }
+    function displayWrong() {
+        var cycle = setTimeout(displayQ, 2000)
+        var messageArea = $("<div>");
+        messageArea.addClass("message-content")
+
+        var lossMessage = $("<h2>");
+
+        messageArea.appendTo($('#content'));
+        lossMessage.appendTo($(messageArea));
+        lossMessage.html("Wrong! The right answer was: " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
+
+
+        if (currentQuestion === (questions.length - 1)) {
+            clearTimeout(cycle);
+            var gameEnd = setTimeout(gameOver, 2000)
+        }
+        currentQuestion++;
+    };
+
+    function timedOut() {
+        var cycle = setTimeout(displayQ, 2000)
+        var messageArea = $("<div>");
+        messageArea.addClass("message-content")
+        var lossMessage = $("<h2>");
+
+        messageArea.appendTo($("#content"));
+        lossMessage.appendTo(messageArea);
+        lossMessage.html("Time is up! The right answer was: " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
+
+        if (currentQuestion === (questions.length - 1)) {
+            clearInterval(cycle);
+            var gameEnd = setTimeout(gameOver, 2000)
+        }
+        currentQuestion++;
+
+    };
+
+    function gameOver() {
+        $(".message-content").remove();
+        var totalCorrect = $("<h3>")
+        var totalIncorrect = $("<h3>")
+        var totalNone = $("<h3>")
+        var restart = $("<button>")
+        totalCorrect.appendTo($("#content"))
+        totalCorrect.html("You got " + correct + " correct!")
+        totalIncorrect.appendTo("#content")
+        totalIncorrect.html("You got " + wrong + " wrong.")
+        totalNone.appendTo("#content")
+
+        if (none === 1) {
+            totalNone.html("You didn't answer  " + none + " question.")
+        }
+        if (none > 1 || none === 0) {
+            totalNone.html("You didn't answer  " + none + " questions.")
+        }
+        restart.addClass("restart")
+        restart.text("Restart")
+        restart.appendTo($("#content"))
+
+        $(".restart").on("click", function () {
+            totalCorrect.remove();
+            totalIncorrect.remove();
+            totalNone.remove();
+            restart.remove();
+            currentQuestion = 0;
+            correct = 0;
+            wrong = 0;
+            none = 0;
+            displayQ();
         })
-                    
-               
-    
+    }
+})
 
 
-            //  display results
-//             var results = document.getElementById('results');
-//             results.innerHTML = "<h3> You scored <span> '+score+' </span> out of <span>'+total+'</span></h3>"   // alert("You scored " +score+ " out of +total+");
-//         }
-
-//         return false;
-
-//     }
-
-// });
-
-// var gameStartTime = 45;
 
 
-// $("#startgame").on("click", function () {
-//     timer = setInterval(gameTimer, 1000);
-//     gameTimer();
-//     console.log(timer);
-//     $("#startgame").remove();
-// })
-// function gameTimer() {
-//     // if gameStartTime >= 0;
-//     gameStartTime--;
-//     $("#timeLeft").html("Time Remaining " + gameStartTime);
-//     console.log(gameStartTime);
-    //  else  {
-    //      if gameStartTime == 0;
-    //         end gameStartTime;
-    //  }  
-        // stop game
